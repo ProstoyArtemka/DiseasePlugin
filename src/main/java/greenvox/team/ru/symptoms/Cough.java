64d698716355bd20baa16a5b6a3f4346e1cac030
White-Zunderer <whitezunder@yandex.ru>
2023-01-18 17:52:05 +0700

    Revised Cough.java and deleted CoughManager.java

--- src/main/java/greenvox/team/ru/symptoms/Cough.java
+++ src/main/java/greenvox/team/ru/symptoms/Cough.java
@@ -1,47 +1,49 @@
 package greenvox.team.ru.symptoms;
 
 import greenvox.team.ru.database.DatabaseManager;
-import greenvox.team.ru.disease.DiseaseManager;
 import greenvox.team.ru.disease.Symptom;
-import greenvox.team.ru.util.CoughManager;
 import org.bukkit.Color;
 import org.bukkit.Particle;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.RayTraceResult;
-
+import org.bukkit.util.Vector;
 
 
 public class Cough implements Symptom {
 
-
-
     @Override
-    public void init(JavaPlugin main) {
-
-    }
+    public void init(JavaPlugin main) { }
 
     @Override
     public void execute(Player player) {
-        Particle.DustOptions colorize = new Particle.DustOptions(Color.fromRGB(109, 249, 203), 0.5F);
+        // Changed name from colorize to color.
+        Particle.DustOptions color = new Particle.DustOptions(Color.fromRGB(109, 249, 203), 0.5F);
 
-        player.spawnParticle(Particle.REDSTONE, player.getLocation().add(0,0.5,0), 5, colorize);
-        CoughManager.pushAway(player, 1);
+        player.spawnParticle(Particle.REDSTONE, player.getEyeLocation(), 5, color);
+        pushAway(player, 0.25f);
 
-        //tracing if player looking at another player
+        // Tracing if player looking at another player (Changed lambda)
         RayTraceResult result = player.getWorld().rayTraceEntities(
                 player.getEyeLocation(),
                 player.getLocation().getDirection(),
                 5,
-                e -> e == player && e instanceof LivingEntity
+                e -> e != player && e instanceof Player
         );
 
-        if (result != null) {
-            Player tracedPlayer = (Player) result.getHitEntity();
+        if (result == null) return;
+        Player tracedPlayer = (Player) result.getHitEntity();
 
-            DatabaseManager.applyDiseaseToPlayer(tracedPlayer);
-        }
+        if (tracedPlayer == null) return;
+        DatabaseManager.applyDiseaseToPlayer(tracedPlayer);
+    }
+
+    // Moved from CoughManager
+    private void pushAway(Player player, double speed) {
+        if (!player.isOnline()) return;
+
+        Vector UnitVector = player.getLocation().getDirection().normalize();
+        player.setVelocity(UnitVector.multiply(-speed));
     }
 
     @Override
