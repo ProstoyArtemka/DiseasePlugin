169d3d3ee7f5c37510d71c2d2a479706500f5fc4
Unillion <114824496+Unillion@users.noreply.github.com>
2023-01-18 20:27:46 +0500

    added Vertigo

--- src/main/java/greenvox/team/ru/symptoms/Vertigo.java
+++ src/main/java/greenvox/team/ru/symptoms/Vertigo.java
@@ -3,6 +3,9 @@ package greenvox.team.ru.symptoms;
 import greenvox.team.ru.disease.Symptom;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 public class Vertigo implements Symptom {
     @Override
@@ -12,7 +15,28 @@ public class Vertigo implements Symptom {
 
     @Override
     public void execute(Player player) {
+        int minSec = 5;
+        int maxSec = 10;
+        int count = 0;
+        int minVelocity = -1;
+        int maxVelocity = 2;
+        int range = maxVelocity - minVelocity + 1;
 
+        Vector playerVelocity = player.getVelocity().normalize();
+
+        int sec = (int) ((Math.random() * (maxSec - minSec)) + minSec);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, sec, 3));
+
+        for (int i = -1; i <= sec; i++) {
+            count++;
+            int random = (int) (Math.random() * range) + minVelocity;
+
+            if (count == 1) {
+                playerVelocity.setX(random);
+            } else if (count == 2) {
+                playerVelocity.setY(random);
+            } else break;
+        }
     }
 
     @Override
