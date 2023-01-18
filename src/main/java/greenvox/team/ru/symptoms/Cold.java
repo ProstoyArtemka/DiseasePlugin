25f21649d049922e1d771f206e9f77927257fb38
Jiuuc <mcakaygarov@gmail.com>
2023-01-18 16:43:57 +0300

    cold

--- src/main/java/greenvox/team/ru/symptoms/Cold.java
+++ src/main/java/greenvox/team/ru/symptoms/Cold.java
@@ -3,6 +3,8 @@ package greenvox.team.ru.symptoms;
 import greenvox.team.ru.disease.Symptom;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Cold implements Symptom {
     @Override
@@ -12,7 +14,10 @@ public class Cold implements Symptom {
 
     @Override
     public void execute(Player player) {
-
+        int min = 5;
+        int max = 16;
+        int sec = (int) ((Math.random() * (max - min)) + min);
+        player.setFreezeTicks(sec*20);
     }
 
     @Override
