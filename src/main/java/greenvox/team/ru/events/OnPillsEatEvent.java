53cd5f72a4e743f8604e2efe9dd67bbede35cceb
Unillion <114824496+Unillion@users.noreply.github.com>
2023-02-14 21:00:07 +0500

    AUUUUGH

--- src/main/java/greenvox/team/ru/events/OnPillsEatEvent.java
+++ src/main/java/greenvox/team/ru/events/OnPillsEatEvent.java
@@ -10,11 +10,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class OnPillsEatEvent implements Listener {
 
     public static final List<String> players = new ArrayList<>();
+    public HashMap<String, Integer> playersLvl = new HashMap<>();
 
     @EventHandler
     public void onEat(PlayerItemConsumeEvent e) {
@@ -22,18 +24,16 @@ public class OnPillsEatEvent implements Listener {
         if (!DatabaseManager.isPlayerIsInfected(player)) return;
         if (players.contains(player.getName())) return;
 
-        int lvl = DatabaseManager.getDiseaseLevelFromPlayer(player);
-
         String message = "&aВаша болезнь отступает";
 
         if (e.getItem().getItemMeta().getPersistentDataContainer().has(PillsCraft.PillsTag)) {
-            DatabaseManager.setDiseaseLevelToPlayer(player, -1);
-
             players.add(player.getName());
+            playersLvl.put(player.getName(), DatabaseManager.getDiseaseLevelFromPlayer(player));
 
+            DatabaseManager.setDiseaseLevelToPlayer(player, -1);
             player.sendActionBar(ChatColor.translateAlternateColorCodes('&', message));
 
-            SchedulerManager.runTaskLater("Pills_run" + player.getName(), new OnPillsRunnable(player, lvl), 10 * 20);
+            SchedulerManager.runTaskLater("Pills_run" + player.getName(), new OnPillsRunnable(player, playersLvl), 10 * 20);
         }
 
     }
--- src/main/java/greenvox/team/ru/events/OnPillsRunnable.java
+++ src/main/java/greenvox/team/ru/events/OnPillsRunnable.java
@@ -1,28 +1,33 @@
 package greenvox.team.ru.events;
 
 import greenvox.team.ru.database.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+
 public class OnPillsRunnable extends BukkitRunnable {
 
-    private static Player player;
-    public static int diseaseLvl;
+    private final Player player;
+    private static HashMap<String, Integer> diseaseLvl;
 
-    public OnPillsRunnable(Player player, int diseaseLvl) {
+    public OnPillsRunnable(Player player, HashMap<String, Integer> diseaseLvl) {
         this.player = player;
-        this.diseaseLvl = diseaseLvl;
+        OnPillsRunnable.diseaseLvl = diseaseLvl;
     }
 
     @Override
     public void run() {
         String message = "&cВам снова становится плохо";
 
-        DatabaseManager.setDiseaseLevelToPlayer(player, diseaseLvl);
+        DatabaseManager.setDiseaseLevelToPlayer(player, diseaseLvl.get(player.getName()));
         player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 10 * 20, 1));
+
+        diseaseLvl.remove(player.getName());
 
         OnPillsEatEvent.players.remove(player.getName());
         player.sendActionBar(ChatColor.translateAlternateColorCodes('&', message));
@@ -30,8 +35,12 @@ public class OnPillsRunnable extends BukkitRunnable {
     }
 
     public static void onDisable() {
-        if (player == null) return;
-            DatabaseManager.setDiseaseLevelToPlayer(player, diseaseLvl);
-        }
+        for (Player player : Bukkit.getOnlinePlayers())
+
+           if (DatabaseManager.getDiseaseLevelFromPlayer(player) < 0) {
+               DatabaseManager.setDiseaseLevelToPlayer(player, diseaseLvl.get(player.getName()));
+           }
+       }
     }
+
 
